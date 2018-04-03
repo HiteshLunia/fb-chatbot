76a5208a1d5d32fccc10a0270fe61ef9e62d6215
hitesh.lunia <lunia.hitesh11@gmail.com>
2018-04-03 23:52:34 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -1,44 +1,55 @@
 import os, sys
 from flask import Flask, request
+from pymessenger import Bot
 
 app = Flask(__name__)
 
-PAGE_ACCESS_TOKEN = ""
+PAGE_ACCESS_TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
 bot = Bot(PAGE_ACCESS_TOKEN)
 
-@app.route('/',methods=['GET'])
+
+@app.route('/', methods=['GET'])
 def verify():
+	# Webhook verification
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
         if not request.args.get("hub.verify_token") == "hello":
-            return "Verification token mismatch",403
-        return request.args["hub.challenge"],200
-    return "Hello World",200
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+    return "Hello world", 200
 
-@app.route('/',methods=['POST'])
+
+@app.route('/', methods=['POST'])
 def webhook():
-    data = request.get_json()
-    log(data)
+	data = request.get_json()
+	log(data)
 
-    if data['object'] == 'page'
-        for entry in data['entry']
-            for messaging_event in entry['messaging']
-                #IDs
-                sender_id = messaging_event['sender']['id']
-                recipient_id = messaging_event['recipient']['id']
+	if data['object'] == 'page':
+		for entry in data['entry']:
+			for messaging_event in entry['messaging']:
 
-                if messaging_event.get('message')
-                if 'text' in messaging_event['message']:
-                    messaging_text = messaging_event['message']['text']
-                else:
-                    messaging_text = 'no text'
+				# IDs
+				sender_id = messaging_event['sender']['id']
+				recipient_id = messaging_event['recipient']['id']
 
-                response = messaging_text
-                bot.send_text_message(sender_id,response)
-    
-    return "ok",200
+				if messaging_event.get('message'):
+					# Extracting text message
+					if 'text' in messaging_event['message']:
+						messaging_text = messaging_event['message']['text']
+					else:
+						messaging_text = 'no text'
+
+					# Echo
+					response = messaging_text
+					bot.send_text_message(sender_id, response)
+
+	return "ok", 200
+
+
 def log(message):
-    print(message)
-    sys.stdout.flush()
+	print(message)
+	sys.stdout.flush()
+
 
 if __name__ == "__main__":
-    app.run(debug = True, port = 80)
+	app.run(debug = True, port = 80)
